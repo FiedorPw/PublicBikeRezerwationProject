0541ec02c35e737972aa00054c486f28e85d3b57
mfiedorc <01168927@pw.edu.pl>
2022-01-17 16:42:45 +0100

    Upload New File

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+	NearestStationFinder finder = new NearestStationFinder();
+        System.out.println(finder.findClosestStation(0,0));
+
+    }
+}
