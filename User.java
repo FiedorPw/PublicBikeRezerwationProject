9df74ac9ff3d3f99e80f439b8dc7c0d9ec4a931a
Julia Parobczy <julia.parobczy.stud@pw.edu.pl>
2022-01-17 21:40:21 +0100

    klasa uzytkownikow

---
+++ User.java
@@ -0,0 +1,77 @@
+public class User {
+
+    private int index;
+    private String name;
+    private String surname;
+    //na razie zapisuje godzine jako Stringa
+    private String bookedAt;
+    private int time;
+    private int coordinateX;
+    private int coordinateY;
+
+    public User(int index, String name, String surname, String bookedAt, int time, int coordinateX, int coordinateY) {
+        this.index = index;
+        this.name = name;
+        this.surname = surname;
+        this.bookedAt = bookedAt;
+        this.time = time;
+        this.coordinateX = coordinateX;
+        this.coordinateY = coordinateY;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getBookedAt() {
+        return bookedAt;
+    }
+
+    public void setBookedAt(String bookedAt) {
+        this.bookedAt = bookedAt;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+    public int getCoordinateX() {
+        return coordinateX;
+    }
+
+    public void setCoordinateX(int coordinateX) {
+        this.coordinateX = coordinateX;
+    }
+
+    public int getCoordinateY() {
+        return coordinateY;
+    }
+
+    public void setCoordinateY(int coordinateY) {
+        this.coordinateY = coordinateY;
+    }
+}
