01fad311799f6e3698913aa9a2ccddfc4eba4cb4
Julia Parobczy <julia.parobczy.stud@pw.edu.pl>
2022-01-30 18:53:25 +0100

    testy

--- Main.java
+++ Main.java
@@ -1,12 +1,11 @@
 public class Main {
     public static void main(String[] args) {
 
-       /* User user = new User(1, "adad", "aD", 12, 12,-500,450);
         MainApp mainApp = new MainApp("stations.csv", "users.csv");
-        mainApp.findClosestStation(user);
         mainApp.usersClosestStations();
+;
 
-        */
+
 
     }
 }
