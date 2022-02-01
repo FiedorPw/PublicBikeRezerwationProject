ddfb30a56d2bd6367ab3d4902aa47ffd44c62327
Julia Parobczy <julia.parobczy.stud@pw.edu.pl>
2022-02-01 10:40:17 +0100

    klasa testowa

--- Main.java
+++ Main.java
@@ -2,9 +2,23 @@ public class Main {
     public static void main(String[] args) {
 
         MainApp mainApp = new MainApp("stations.csv", "users.csv");
+        //wyświetlanie stacji dla użytkownika wraz z liczbą pozostałych rowerów na stacji
         mainApp.usersClosestStations();
-        mainApp.renting();
-        mainApp.writingCSV();
+
+        //zapisywanie danych do pliku capacity.csv
+        mainApp.writingCSV("capacity.csv");
+
+        //wyświetlanie aktualnego stanu wszystkich stacji
+        mainApp.allStationsCapacity();
+
+        //zwracanie wszystkich rowerów
+        mainApp.returnAllBikes();
+
+        //wyświetlanie stanu stacji po zwróceniu
+        mainApp.allStationsCapacity();
+
+        //późniejszy zapis do pliku
+        mainApp.writingCSV("capacity2.csv");
 
 
 
